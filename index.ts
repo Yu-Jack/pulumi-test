88e974b32f7883cedac4c18ae64c1eb11621b750
Yu-Jack <jk82421@gmail.com>
2022-02-23 11:03:33 +0800

    test

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 // Create an AWS resource (S3 Bucket)
-// blabla12s
+// blabla12sasd
 const bucket = new aws.s3.Bucket("my-test-bucket", {
     website: {
         indexDocument: "index.html",
